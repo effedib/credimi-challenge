2b7a744813023a63870300a2f229cbc3c2620621
Francesco <cicciodb@hotmail.it>
2022-04-26 00:04:42 +0200

    added type control

--- utils/utils.py
+++ utils/utils.py
@@ -9,6 +9,9 @@ If the 'expire date' is > (today + 30 days), the status will be 'Valid'.
 def check_status_doc(expire_date):
     from datetime import date, timedelta
 
+    if type(expire_date) != date:
+        raise TypeError('Incorrect date input format: {}'.format(expire_date))
+
     if (expire_date - date.today()) > timedelta(days=30):
         status = 'Valid'
     elif timedelta(days=1) <= (expire_date - date.today()) <= timedelta(days=30):
@@ -21,10 +24,16 @@ def check_status_doc(expire_date):
 
 # Check if the fiscal code has a correct format.
 def is_valid_fiscal_code(fiscal_code):
-    pass
+    from re import compile, search
+    pattern = compile(r'^[A-Za-z]{6}[0-9]{2}[A-Za-z]{1}[0-9]{2}[A-Za-z]{1}[0-9]{3}[A-Za-z]{1}$')
+    if search(pattern, fiscal_code):
+        return True
+
+    return False
 
 
 if __name__ == '__main__':
+    print(check_status_doc('1234'))
     print(is_valid_fiscal_code('DBLFNC69P52D994G'))
     print(is_valid_fiscal_code('1BLFNC69P52D994G'))
     print(is_valid_fiscal_code('BLFNC69P52994'))
